3e891a8bb771b404bc2ba6623cdc383e63439d94
Phil Arfuso <Arfusop@gmail.com>
2019-04-30 22:43:29 -0400

    Connected to mongoDB.

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
+
+// connect DB
+connectDB();
 
 app.get("/", (req, res) => res.send("Api running."));
 
