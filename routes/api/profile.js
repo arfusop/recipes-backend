c4fac997279df104ff83facf4a97fbb176c9ee05
Your Name <arfusop@gmail.com>
2019-05-06 20:05:23 -0400

    Added get route for all profiles, and for profile specific to user id.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -119,4 +119,26 @@ router.get("/", async (req, res) => {
 	}
 });
 
+// @route   GET api/profile/user/:user_id
+// @desc    GET profile by user ID
+// @access  Public
+router.get("/user/:user_id", async (req, res) => {
+	try {
+		const profile = await Profile.findOne({
+			user: req.params.user_id
+		}).populate("user", ["firstName", "lastName"]);
+
+		if (!profile)
+			return res.status(400).json({ msg: "Profile not found." });
+
+		res.json(profile);
+	} catch (err) {
+		console.error(err);
+		if (err.kind == "ObjectId") {
+			return res.status(400).json({ msg: "Profile not found." });
+		}
+		res.status(500).send("Server Error");
+	}
+});
+
 module.exports = router;
